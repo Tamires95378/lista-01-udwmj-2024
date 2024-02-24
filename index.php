9cbd2803139675a476307f5e8c7f839da4eba8c9
Tamires Rodrigues <rodriguestamires510@gmail.com>
2024-02-24 13:01:24 -0300

    Initial
    Me apresentando

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá, Mundo!</title>
+</head>
+<body>
+    <h1> Um pouco de mim </h1>
+    <?php 
+    echo "Olá, Mundo! \u{1F5FA}";
+    ?>
+    <p> Me chamo Tamires Rodrigues, tenho 22 anos, estou cursando Análise e Desenvolvimento de Sistemas na Una-Betim/MG, estou no último período, no momento estou aprendendo a liguagem de programação PHP. </p>
+</body>
+</html>
